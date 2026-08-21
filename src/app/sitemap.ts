a8c1bad9b3af4aaa3f5973bc762b761d06a89ce9
Matheus Carvalho <henriquematheus.c@gmail.com>
2026-08-21 13:42:41 -0300

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,9 +5,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${process.env.SITE_URL}`,
     },
-    {
-      url: `${process.env.SITE_URL}/politica-de-privacidade`,
-    },
   ];
 
   return sitemap;
